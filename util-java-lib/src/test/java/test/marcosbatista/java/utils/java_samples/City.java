9802cf3605e2a74422b0fdbbbcb9959f66ab32c4
Marcos Batista <mab.marcos@gmail.com>
2025-07-30 18:46:32 -0300

    Delete util-java-lib directory

--- util-java-lib/src/test/java/test/marcosbatista/java/utils/java_samples/City.java
+++
@@ -1,8 +0,0 @@
-package test.marcosbatista.java.utils.java_samples;
-
-public interface City {
-	
-	public void setProvince(Province province);
-	public Province getProvince();
-	
-}
--- util-java-lib/src/test/java/test/marcosbatista/java/utils/java_samples/Province.java
+++
@@ -1,8 +0,0 @@
-package test.marcosbatista.java.utils.java_samples;
-
-public interface Province {
-	
-	public void setAcronym(String acronym);
-	public String getAcronym();
-	
-}
