bf44782d3d29129b99406d93405cbd1a81ce4990
Marcos Batista <mab.marcos@gmail.com>
2025-07-29 23:19:28 -0300

    Update Country.java

--- mb-util-java/src/test/java/test/marcosbatista/java/utils/samples/Country.java
+++ mb-util-java/src/test/java/test/marcosbatista/java/utils/samples/Country.java
@@ -2,11 +2,9 @@ package test.marcosbatista.java.utils.samples;
 
 public interface Country {
 	
-	/** name **/
 	public void setName(String name);
 	public String getName();
 	
-	/** province **/
 	public void setProvince(Province province);
 	public Province getProvince();
 	
