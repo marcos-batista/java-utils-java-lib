02f139c20c0274b51cdad5aa7f5e34cc96ac4498
Marcos Batista <mab.marcos@gmail.com>
2025-07-29 22:38:22 -0300

    Adding new sample "City"

---
+++ mb-util-java/src/test/java/test/marcosbatista/java/utils/samples/CityImpl.java
@@ -0,0 +1,15 @@
+package test.marcosbatista.java.utils.samples;
+
+public class CityImpl implements City {
+	
+	private Province province;
+	
+	public void setProvince(Province province) {
+		this.province = province;
+	}
+	
+	public Province getProvince() {
+		return province;
+	}
+	
+}
